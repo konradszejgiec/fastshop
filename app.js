33c8c9ae1bd1cce06ed2f302ac124f70f71e884d
Konrad Szejgiec <konrad.szejgiec@gmail.com>
2021-08-31 22:37:33 +0200

    Small updates

--- app.js
+++ app.js
@@ -11,18 +11,21 @@ const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   -process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database connection successful."));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+(async function connectToDB() {
+  await mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connection successful."));
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})();
 
 app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "pug");
